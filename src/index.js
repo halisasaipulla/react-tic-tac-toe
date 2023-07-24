4b9194a99101a33a8abf2bf3692f95c01ff44f26
Lisa S <halisasaipulla@gmail.com>
2023-07-23 17:32:53 -0700

    edited app name

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// ReactDOM.createRoot(
+//     document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
